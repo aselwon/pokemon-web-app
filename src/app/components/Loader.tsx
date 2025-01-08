dadc28bd2d312d7c148511a3b3ffd86160abca7d
Adam Selwon <adamselwon1993@gmail.com>
2025-01-08 19:19:10 +0100

    Add styles for Loader

--- src/app/components/Loader.tsx
+++ src/app/components/Loader.tsx
@@ -1,7 +1,11 @@
 import React, { JSX } from "react"
 
 const Loader = (): JSX.Element => {
-  return <div>Loading</div>
+  return (
+    <div className="bg-white min-h-screen flex flex-col items-center justify-center text-black">
+      Loading...
+    </div>
+  )
 }
 
 export default Loader
