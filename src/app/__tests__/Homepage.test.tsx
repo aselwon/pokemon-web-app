d129148c748f89a7eff410c4315546d0b30e13dc
Adam Selwon <adamselwon1993@gmail.com>
2025-01-08 20:24:32 +0100

    Init setup for tests and add tests for home page

---
+++ src/app/__tests__/Homepage.test.tsx
@@ -0,0 +1,44 @@
+import { render, screen } from "@testing-library/react"
+import HomePage from "@/app/page"
+
+jest.mock("next/router", () => ({
+  useRouter: jest.fn(() => ({
+    route: "/",
+    pathname: "/",
+    query: {},
+    asPath: "/",
+    push: jest.fn()
+  }))
+}))
+
+jest.mock("../components/SearchForm", () => () => {
+  return <form role="form">Mocked SearchForm</form>
+})
+
+describe("HomePage Component", () => {
+  it("should render the Pokemon logo image", () => {
+    render(<HomePage />)
+    const logoImage = screen.getByTestId("homepage-image")
+    expect(logoImage).toBeInTheDocument()
+  })
+
+  it("should render the welcome message with title and subtitle", () => {
+    render(<HomePage />)
+    const titleElement = screen.getByTestId("homepage-title")
+    const subtitleElement = screen.getByTestId("homepage-subtitle")
+
+    expect(titleElement).toBeInTheDocument()
+    expect(subtitleElement).toBeInTheDocument()
+
+    expect(titleElement).toHaveTextContent("Welcome to Pokemon App!")
+    expect(subtitleElement).toHaveTextContent(
+      "Use the search bar above to find your favorite Pokemon."
+    )
+  })
+
+  it("should render the SearchForm component", () => {
+    render(<HomePage />)
+    const searchFormElement = screen.getByRole("form", { hidden: true })
+    expect(searchFormElement).toBeInTheDocument()
+  })
+})
