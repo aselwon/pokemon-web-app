fa862448842194fd2171990981be1c4cbed1fd53
Adam Selwon <adamselwon1993@gmail.com>
2025-01-08 12:02:22 +0100

    Add not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+export default function NotFound() {
+  return (
+    <div className="bg-white min-h-screen flex flex-col items-center justify-center text-black gap-5">
+      <img src="/pikachu.png" alt="Pikachu" />
+      <h1>404 - Page Not Found</h1>
+      <p>Pokemon not found. Try searching for another one!</p>
+      <Link href="/" className="bg-blue-500 text-white rounded-lg px-4 py-2">
+        Go back to homepage
+      </Link>
+    </div>
+  )
+}
